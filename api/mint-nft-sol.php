e0a3ef8443a11a2e556d7abf9f594d12ca849813
tr1h <travkevich@gmail.com>
2025-11-11 19:56:34 +0300

    Fix OPTIONS handling in mint-nft-sol.php

--- api/mint-nft-sol.php
+++ api/mint-nft-sol.php
@@ -54,8 +54,10 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-W
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Max-Age: 86400');
 
+// Handle preflight requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
+    header('Content-Length: 0');
     exit();
 }
 
