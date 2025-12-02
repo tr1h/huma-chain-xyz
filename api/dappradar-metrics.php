847b8a42609b62b22432caf58cd872e08cf3c0fa
tr1h <travkevich@gmail.com>
2025-12-02 23:47:37 +0300

    Fix: Add Cache-Control to CORS headers in dappradar-metrics.php

--- api/dappradar-metrics.php
+++ api/dappradar-metrics.php
@@ -13,7 +13,7 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type, Cache-Control, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
